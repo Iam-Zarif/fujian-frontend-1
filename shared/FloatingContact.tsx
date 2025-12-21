987e7a5b37da3adb3b4bb6302e941dcd3e80a82a
Iam-Zarif <zarifraja19@gmail.com>
2025-12-21 16:31:33 +0600

    logo

--- shared/FloatingContact.tsx
+++ shared/FloatingContact.tsx
@@ -2,37 +2,36 @@
 
 import { memo } from "react";
 import Link from "next/link";
-import { PhoneCall, Send } from "lucide-react";
+import { PhoneCall, Mail } from "lucide-react";
+import { FaWhatsapp } from "react-icons/fa";
 
 function FloatingContact() {
   return (
-    <div className="fixed lg:bottom-5 bottom-2 right-2 lg:right-5 z-999 flex flex-col gap-3">
+    <div className="fixed bottom-2 right-2 z-50 flex flex-col gap-3 lg:bottom-5 lg:right-5">
       <Link
-        href="tel:+1234567890"
-        aria-label="Call Us"
-        className="relative flex h-10 lg:w-12 w-10 lg:h-12  bg-white items-center justify-center rounded-2xl 
-         backdrop-blur-xl 
-        ring-1 ring-black/5
-        shadow-[0_6px_16px_rgba(0,0,0,0.14)]
-        transition-all hover:-translate-y-0.5"
+        href="tel:+8801714813840"
+        aria-label="Call Fujian Accessories"
+        className="relative flex h-10 w-10 items-center justify-center rounded-2xl bg-white backdrop-blur-xl
+        ring-1 ring-black/5 shadow-[0_6px_16px_rgba(0,0,0,0.14)]
+        transition-all hover:-translate-y-0.5 lg:h-12 lg:w-12"
       >
         <span className="absolute inset-0 rounded-2xl bg-[#00019A]/25 blur-xl" />
-        <PhoneCall className="relative lg:h-6 lg:w-6 h-4 w-4 text-[#00019A]" />
+        <PhoneCall className="relative h-4 w-4 text-[#00019A] lg:h-6 lg:w-6" />
       </Link>
 
       <Link
-        href="https://wa.me/XXXXXXXXXX"
+        href="https://wa.me/8801714813840"
         target="_blank"
-        aria-label="WhatsApp"
-        className="relative flex h-10 lg:w-12 w-10 lg:h-12 bg-white  items-center justify-center rounded-2xl 
-         backdrop-blur-xl 
-        ring-1 ring-black/5
-        shadow-[0_6px_16px_rgba(0,0,0,0.14)]
-        transition-all hover:-translate-y-0.5"
+        aria-label="WhatsApp Fujian Accessories"
+        className="relative flex h-10 w-10 items-center justify-center rounded-2xl bg-white backdrop-blur-xl
+        ring-1 ring-black/5 shadow-[0_6px_16px_rgba(0,0,0,0.14)]
+        transition-all hover:-translate-y-0.5 lg:h-12 lg:w-12"
       >
         <span className="absolute inset-0 rounded-2xl bg-[#25D366]/25 blur-xl" />
-        <Send className="relative  lg:h-6 lg:w-6 h-4 w-4 text-[#25D366]" />
+        <FaWhatsapp className="relative h-4 w-4 text-[#25D366] lg:h-6 lg:w-6" />
       </Link>
+
+    
     </div>
   );
 }
